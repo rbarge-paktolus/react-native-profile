c1264b479e11b758221cc8212e996b99efeb09c8
rbarge-paktolus <rbarge@paktolus.com>
2023-07-24 11:53:53 +0530

    add list to dashboard

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -3,21 +3,41 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 
 const profilePicture = require('../assets/profile.png');
 
+const sampleData = [
+    { id: '1', title: 'Item 1' },
+    { id: '2', title: 'Item 2' },
+    { id: '3', title: 'Item 3' },
+    // Add more items as needed
+];
+
 const Dashboard = () => {
   return (
-      <View style={styles.container}>
-          {/* Left section for dashboard content */}
-          <View style={styles.contentContainer}>
-              {/* Add your dashboard content here */}
-              <Text style={styles.dashboardText}>Welcome to the Dashboard!</Text>
-              {/* Add other components and content */}
-          </View>
+    <View>
+          <View style={styles.container}>
+              {/* Left section for dashboard content */}
+              <View style={styles.contentContainer}>
+                  {/* Add your dashboard content here */}
+                  <Text style={styles.dashboardText}>Welcome to the Dashboard!</Text>
+                  {/* Add other components and content */}
+              </View>
 
-          {/* Right section for user profile picture */}
-          <View style={styles.profileContainer}>
-              <Image source={profilePicture} style={styles.profileImage} />
+              {/* Right section for user profile picture */}
+              <View style={styles.profileContainer}>
+                  <Image source={profilePicture} style={styles.profileImage} />
+              </View>
           </View>
-      </View>
+          <View>
+              <FlatList
+                  data={sampleData}
+                  renderItem={({ item }) => (
+                      <View style={styles.listItem}>
+                          <Text>{item.title}</Text>
+                      </View>
+                  )}
+                  keyExtractor={item => item.id}
+              />
+          </View>
+    </View>
   );
 };
 
